c4d6c63edaaa60a0b64f958db1adab7e8508bf74
Alejandro Fernández Suárez <alejandro.fernandez.suarez@gmail.com>
2022-10-03 17:15:48 +0100

    terminate read feed example on stdin close

--- examples/read_feed.cpp
+++ examples/read_feed.cpp
@@ -1,8 +1,16 @@
 #include <cstdlib>
+#include <future>
 #include <glog/logging.h>
 #include <iostream>
 
 #include "coinbase/client.hpp"
+
+// Whether the given future is ready.
+template <class T>
+bool ready(const std::future<T> &f,
+           std::chrono::seconds timeout = std::chrono::seconds(0)) {
+  return f.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
+}
 
 int main(int argc, char **argv) {
   google::InitGoogleLogging(argv[0]);
@@ -13,12 +21,14 @@ int main(int argc, char **argv) {
     // Make a feed for all products.
     std::unique_ptr<coinbase::feed> feed = client.make_feed(client.products());
 
-    // Consume it indefinitely.
-    for (;;) {
+    // Consume it until the user stops.
+    LOG(INFO) << "Consuming feed until stdin is closed.";
+    std::future<void> async_input = std::async([]() { std::cin.get(); });
+    while (!ready(async_input)) {
       std::cout << feed->next() << std::endl;
     }
   } catch (std::exception const &e) {
-    std::cerr << "Error: " << e.what() << std::endl;
+    LOG(ERROR) << "Error: " << e.what();
     return EXIT_FAILURE;
   }
 
